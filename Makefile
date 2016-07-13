61cfee96d715bce4e3ce5af935463c4f4ae6db46
Gabriel <gabriel.cardoso@corp.globo.com>
2016-07-13 14:00:39 -0300

    updates requirements, adds dev-only requirements and new template

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ clean:
 	find . -name \*pyc -delete
 
 install:
-	pip install -r requirements.txt
+	pip install -r requirements-dev.txt
 
 testserver: clean
 	FLASK_DEBUG=1 FLASK_APP=core/appserver.py flask run
--- core/appserver.py
+++ core/appserver.py
@@ -7,5 +7,15 @@ app = flask.Flask(__name__)
 def hello():
     return "ok we're running!"
 
+
+@app.route("/upload")
+def upload_view:
+    return "upload_view"
+
+
+@app.route("/tv")
+def gifboard_view:
+    return "gifboard_view"
+
 if __name__ == "__main__":
     app.run()
