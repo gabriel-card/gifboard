2927abccbc6e1b65b334660db2b0838e29ae6fbc
Gabriel <gabriel.cardoso@corp.globo.com>
2016-07-19 15:45:37 -0300

    adds compass config file

---
+++ config.rb
@@ -0,0 +1,8 @@
+css_dir = "static/css"
+sass_dir = "assets/scss"
+javascripts_dir = "static/js"
+http_path = "/"
+relative_assets = true
+
+# Specify the output style/environment
+environment = :production
