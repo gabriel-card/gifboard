9e4775af61bafdf5254a8ad4e176cb7ec95e1d97
Gabriel <gabriel.cardoso@corp.globo.com>
2016-08-03 15:02:09 -0300

    adds test to POST method for board page

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,12 +1,19 @@
 # coding: utf-8
 import unittest
+import mock
 from core.appserver import app
 
 
 class RoutesTestCase(unittest.TestCase):
 
     def setUp(self):
+        img_from_url_patcher = mock.patch('core.appserver.upload_img_from_url')
+        save_url_patcher = mock.patch('core.appserver.save_url')
+        self.fake_img_from_url = img_from_url_patcher.start()
+        self.fake_save_url = save_url_patcher.start()
         self.app = app.test_client()
+        self.addCleanup(self.fake_img_from_url.stop)
+        self.addCleanup(self.fake_save_url.stop)
 
     def test_healthcheck(self):
         response = self.app.get('/healthcheck')
@@ -19,6 +26,17 @@ class RoutesTestCase(unittest.TestCase):
     def test_board(self):
         response = self.app.get('/board')
         self.assertEqual(response.status_code, 200)
+
+    def test_post_board(self):
+        response = self.app.post('/board', data=dict(
+            image_url='http://imgur.com/test.gif'
+        ))
+        self.fake_img_from_url.assert_called_once_with(
+           'http://imgur.com/test.gif'
+        )
+        self.fake_save_url.assert_called_once_with(
+           self.fake_img_from_url.return_value
+        )
 
     def test_upload(self):
         response = self.app.get('/upload')
